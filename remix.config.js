13f42bdc3a1d9fcc2ee2306d5f438d03c9192683
Elia Pellegrino <elia@piramid.studio>
2023-09-21 15:14:18 +0200

    upgrage to v2

--- remix.config.js
+++ remix.config.js
@@ -4,13 +4,13 @@ const routes = require('./app/pages/_routes.js')
 
 module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
+  // future: {
+  //   v2_dev: true,
+  //   v2_errorBoundary: true,
+  //   v2_meta: true,
+  //   v2_normalizeFormMethod: true,
+  //   v2_routeConvention: true,
+  // },
   ignoredRouteFiles: ['**/.*'],
   postcss: true,
   serverModuleFormat: 'cjs',
