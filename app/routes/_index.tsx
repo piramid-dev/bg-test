102f10eb8504f3300167924c26263bf52c00ccb6
Elia Pellegrino <elia@piramid.studio>
2023-09-21 16:06:41 +0200

    restore routes folder

---
+++ app/routes/_index.tsx
@@ -0,0 +1,12 @@
+// import { redirect, type LoaderArgs } from '@remix-run/node'
+
+// import { getLocale } from '~/utils'
+
+// export const loader = async ({ request }: LoaderArgs) => {
+//   const locale = getLocale(request)
+//   return locale === 'it' ? redirect('/it/skialper') : redirect('/skialper')
+// }
+
+export default function Index() {
+  return <div>Hub page!!</div>
+}
